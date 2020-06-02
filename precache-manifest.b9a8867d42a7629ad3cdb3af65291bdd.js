c90bc927c073c229517b0389e7984f0405dcc648
Calum Bradbury <eeycb2@nottingham.ac.uk>
2020-06-02 11:16:37 +0100

    Updates

---
+++ precache-manifest.b9a8867d42a7629ad3cdb3af65291bdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c82632ee08adb664c5cffbaa392ffad",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "841e6f8063c8ded9fc91",
+    "url": "/monsters-rolodex/static/css/main.e5783109.chunk.css"
+  },
+  {
+    "revision": "275b5bd9f31198a331b8",
+    "url": "/monsters-rolodex/static/js/2.a67a3063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a67a3063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841e6f8063c8ded9fc91",
+    "url": "/monsters-rolodex/static/js/main.f3d71de5.chunk.js"
+  },
+  {
+    "revision": "647f06759b25837e4d32",
+    "url": "/monsters-rolodex/static/js/runtime-main.d3ed8291.js"
+  }
+]);
